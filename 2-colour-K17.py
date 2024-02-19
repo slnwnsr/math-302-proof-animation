512b31108f1608ba274f8d35ef472be40015665c
Sloane <git@sloaney.ca>
2024-02-18 21:36:04 -0800

    Shows K17

--- 2-colour-K17.py
+++ 2-colour-K17.py
@@ -1,5 +1,8 @@
 from manim import *
+from itertools import combinations
 
+
+# scene just to try out stuff
 class MovingVertices2(Scene):
     def construct(self):
         vertices = [1, 2, 3, 4, 5, 6]
@@ -11,4 +14,19 @@ class MovingVertices2(Scene):
         g_group = Group(g, g2).move_to(LEFT * 3)
         self.play(FadeIn(g_group))
         self.play(g2.animate.next_to(g, RIGHT, buff=1))
+        self.wait()
+
+class K17(Scene):
+    def construct(self):
+
+        vertices = list(range(1,18))
+        arr = []
+        for x in range(1,18):
+            arr.append(x)
+        edges = list(combinations(arr, 2))
+
+        g = Graph(vertices, edges, layout='circular', layout_scale=3.5)
+
+        self.wait()
+        self.play(FadeIn(g))
         self.wait()
