f7dac86d0df272b5dc0ed592b518f4c4d432b1ff
Sloane <git@sloaney.ca>
2024-02-22 23:52:09 -0800

    Added 2-colouring of K17 and sampleScene to test random stuff

--- part-2.py
+++ part-2.py
@@ -16,17 +16,35 @@ class MovingVertices2(Scene):
         self.play(g2.animate.next_to(g, RIGHT, buff=1))
         self.wait()
 
+# GRAPHS------------------------------------------------------
+# edges in K17
+vertices = list(range(0,17))
+arr = []
+for x in range(0,17):
+    arr.append(x)
+edges = list(combinations(arr, 2))
+
+# edges coloured red
+redes = []
+reddiffs = [1, 2, 4, 8, 9, 13, 15, 16]
+for x in edges:
+    if (x[1] - x[0]) in reddiffs:
+        redes.append(x)
+
+# edges coloured blue
+bluees = []
+for x in edges:
+    if x not in redes:
+        bluees.append(x)
+
+g = Graph(vertices, edges, layout='circular', layout_scale=3)
+redg = Graph(vertices, redes, layout='circular', layout_scale=3).set_color(color=RED)
+blueg = Graph(vertices, bluees, layout='circular', layout_scale=3).set_color(color=BLUE)
+# ----------------------------------------------------------
+
 # draws K17 into the scene
 class K17(Scene):
     def construct(self):
-
-        vertices = list(range(1,18))
-        arr = []
-        for x in range(1,18):
-            arr.append(x)
-        edges = list(combinations(arr, 2))
-
-        g = Graph(vertices, edges, layout='circular', layout_scale=3.3)
 
         self.wait()
         self.play(ChangeSpeed(
@@ -34,6 +52,48 @@ class K17(Scene):
                 speedinfo={0.08: 0.08},
                 affects_speed_updaters=True,
             ))
+        # self.play(g.animate.next_to(g, LEFT, buff=-3.3))
         self.wait()
-        self.play(g.animate.next_to(g, LEFT, buff=-3.3))
+
+
+# draws the red and blue edges of K17 then merges them
+class colouredK17(Scene):
+    def construct(self):
+        self.play(FadeIn(g))
         self.wait()
+
+        self.play(ChangeSpeed(
+                AnimationGroup(Create(redg)),
+                speedinfo={0.1: 0.1},
+                affects_speed_updaters=True,
+            ))
+        # self.play(Create(redg))
+        self.wait()
+        self.play(redg.animate.scale(0.6).move_to(RIGHT*4.8))
+        self.wait()
+
+        self.play(ChangeSpeed(
+                AnimationGroup(Create(blueg)),
+                speedinfo={0.1: 0.1},
+                affects_speed_updaters=True,
+            ))
+        # self.play(Create(blueg))
+        self.wait()
+        self.play(blueg.animate.scale(0.6).move_to(LEFT*4.8))
+        self.wait()
+
+        self.play(FadeOut(g))
+        self.wait()
+
+
+        self.play(redg.animate.scale(1.6).move_to([0,0,0]))
+        self.play(blueg.animate.scale(1.6).move_to([0,0,0]))
+        self.wait()
+
+class sampleScene(Scene):
+    def construct(self):
+        blob = Circle(1.5)
+        self.wait()
+        self.play(blob.animate.move_to(RIGHT*4))
+        self.wait()
+        self.play(blob.animate.move_to([0,0,0]))
